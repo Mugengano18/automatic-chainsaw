ebd271d9d468bf15a1e5ffd85154b4e27f769144
Mugengano18 <mugenganoa@gmail.com>
2022-11-25 02:16:26 +0200

    retreived the data from the api

--- carmed/views.py
+++ carmed/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 import requests
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect, reverse
 from folium import plugins
 
@@ -43,17 +43,17 @@ def signUser(request):
 
 
 def loginUser(request):
-    if request.user.is_authenticated:
-        return redirect(reverse("home"))
     if request.method == 'POST':
         phone = request.POST['phone']
         pswd1 = request.POST['pswd1']
 
         user_login = authenticate(username=phone, password=pswd1)
         print(user_login)
+
         if user_login:
             login(request, user_login)
-            return redirect('home')
+            next = request.POST.get('next', '/')
+            return HttpResponseRedirect(next)
         else:
             messages.error(request, "Username or password incorrect!!")
             return redirect('login')
@@ -66,6 +66,27 @@ def logoutUser(request):
     return redirect('login')
 
 
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "city": response.get("city"),
+        "region": response.get("region"),
+        "country": response.get("country_name"),
+        "latitude": response.get("lat")
+    }
+    return location_data
+
+
+print(get_location())
+
+
 def shop_register(request):
     if request.method == 'POST':
         # make requests from api.ipify.org
@@ -74,8 +95,6 @@ def shop_register(request):
         # make requests from ip-api.com
         res = requests.get('http://ip-api.com/json/' + ip_data["ip"])
         data_one = json.loads(res.text)
-        print("ip data", ip_data)
-        print("data_one", data_one)
 
         ret = Business(name=request.POST["fullname"], business_name=request.POST["businessname"],
                        email=request.POST["email"], phone_number=request.POST["phone"],
@@ -99,9 +118,8 @@ def popup_html(row):
     phone_number = i["phone_number"]
     business_type = i["business_type"]
     city = i["city"]
-    district =i["district"]
+    district = i["district"]
     sector = i["sector"]
-    print(name)
     left_col_color = "#3e95b5"
     right_col_color = "#f2f9ff"
 
@@ -116,23 +134,23 @@ def popup_html(row):
     </tr>
     <tr>
     <td style="background-color: """ + left_col_color + """;"><span style="color: #ffffff;">Business Type: </span></td>
-    <td style="width: 150px;background-color: """ + right_col_color + """;">{}</td>"""+ business_type + """
+    <td style="width: 150px;background-color: """ + right_col_color + """;">""" + business_type + """</td>
     </tr>
     <tr>
     <td style="background-color: """ + left_col_color + """;"><span style="color: #ffffff;">Phone Number: </span></td>
-    <td style="width: 150px;background-color: """ + right_col_color + """;">{}</td>"""+ phone_number + """
+    <td style="width: 150px;background-color: """ + right_col_color + """;">""" + phone_number + """</td>
     </tr>
     <tr>
     <td style="background-color: """ + left_col_color + """;"><span style="color: #ffffff;">City: </span></td>
-    <td style="width: 150px;background-color: """ + right_col_color + """;">{}</td>""" + city + """
+    <td style="width: 150px;background-color: """ + right_col_color + """;">""" + city + """</td>
     </tr>
     <tr>
     <td style="background-color: """ + left_col_color + """;"><span style="color: #ffffff;">District: </span></td>
-    <td style="width: 150px;background-color: """ + right_col_color + """;">{}</td>"""+ district + """
+    <td style="width: 150px;background-color: """ + right_col_color + """;">""" + district + """</td>
     </tr>
     <tr>
     <td style="background-color: """ + left_col_color + """;"><span style="color: #ffffff;">Sector: </span></td>
-    <td style="width: 150px;background-color: """ + right_col_color + """;">{}</td>"""+sector + """
+    <td style="width: 150px;background-color: """ + right_col_color + """;">""" + sector + """</td>
     </tr>
     </tbody>
     </table></center>
@@ -143,6 +161,12 @@ def popup_html(row):
 
 @login_required(login_url='login')
 def map_location(request):
+    # make requests from api.ipify.org
+    ip = requests.get('https://api.ipify.org?format=json')
+    ip_data = json.loads(ip.text)
+    # make requests from ip-api.com
+    res = requests.get('http://ip-api.com/json/' + ip_data["ip"])
+    data_one = json.loads(res.text)
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
@@ -154,28 +178,26 @@ def map_location(request):
     location = geocoder.osm(address)
     country = location.country
     map1 = folium.Map(location=[-1.952183, 30.054957], zoom_start=10, tiles='OpenStreetMap')
+    folium.Marker([-1.9499, 30.0588], tooltip='Click for more', popup=data_one["city"],
+                  icon=folium.Icon(color="red", icon='home', prefix='fa')).add_to(map1)
     data_list = Business.objects.all().values()
-    for i in data_list:
-        bus_type = i["business_type"]
+    for i in range(len(data_list)):
+
+        bus_type = data_list[i]['business_type']
         if bus_type == 'Gas station':
             color = 'orange'
         elif bus_type == 'repair shop':
             color = 'gray'
         else:
             color = 'black'
-        html = popup_html(i)
-        print(html)
+        html = popup_html(data_list[i])
         popup = folium.Popup(folium.Html(html, script=True), max_width=500)
-        labels = i["business_name"]
-        lat = i["latitude"]
-        long = i["longitude"]
-        sector = i["sector"]
-        folium.Marker([lat, long], tooltip='Click for more', popup=popup, icon=folium.Icon(color=color, icon='car', prefix='fa')).add_to(map1)
-
-    if lat is None or long is None:
-        address.delete()
-        return HttpResponse('Your address input is incorrect')
-
+        labels = data_list[i]['business_name']
+        lat = data_list[i]['latitude']
+        long = data_list[i]['longitude']
+        sector = data_list[i]['sector']
+        folium.Marker([lat, long], tooltip='Click for more', popup=popup,
+                      icon=folium.Icon(color=color, icon='car', prefix='fa')).add_to(map1)
     map1 = map1._repr_html_()
 
     context = {
